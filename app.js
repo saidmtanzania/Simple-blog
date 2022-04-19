543498e4a193e311fc12116c4265653316dd4839
saidmtanzania <saidmtanzania@gmail.com>
2022-04-19 17:03:22 +0300

    initial changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,7 +17,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-  let POST=[];
+mongoose.connect('mongodb+srv://admin-saidmtanzania:said1561@cluster0.2uzik.mongodb.net/todolistDB',{useNewUrlParser:true});
+const postSChema = {
+  tile: String,
+  content: String
+};
+
+const Post = mongoose.model("Post",postSChema);
+
+let POST=[];
 
 app.get("/",(req,res)=>{
   res.render("home",{StartContent:homeStartingContent ,anotherContent:POST});
@@ -34,11 +43,11 @@ app.get("/compose",(req,res)=>{
 });
 
 app.post("/compose",(req,res)=>{
-  const post={
+  const post= new Post({
     title:req.body.postTitle,
     content:req.body.postBody
-  };
-  POST.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
 app.get("/post/:topic", (req,res)=>{
@@ -50,6 +59,7 @@ app.get("/post/:topic", (req,res)=>{
     };
   });
 });
-app.listen(3000, function() {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
