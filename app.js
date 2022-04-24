08025154b8d32f62691930c2cdf1d4bdaf71deae
saidmtanzania <saidmtanzania@gmail.com>
2022-04-24 17:22:23 +0300

    finial changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-saidmtanzania:said1561@cluster0.2uzik.mongodb.net/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://<enteryourservernameofmongodb></enteryourservernameofmongodb>:<enteryourpassword>@cluster0.2uzik.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
